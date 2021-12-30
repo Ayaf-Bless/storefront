7704fecb1502dc3989500a17c4427d5e897d6176
ayaf-bless <blessambel1@gmail.com>
2021-12-30 16:45:46 +0300

    Complex Lookups Using Q Objects

--- playgroud/views.py
+++ playgroud/views.py
@@ -1,10 +1,12 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from django.shortcuts import render
 from store.models import Product
+
 
 # Create your views here.
 
 def say_hello(request):
-    product = Product.objects.filter(description__isnull=True)
+    product = Product.objects.filter(Q(inventory__lt=10) | ~Q(unit_price__lt=20))
 
     return render(request, "hello.html", {"name": "ayaf", "products": list(product)})
